fdeac12a2e6c98863d857985dc743e48cb1d0b60
prashanth-g <prashanthcsgce@gmail.com>
2018-10-27 23:41:44 +0530

    Added parameterized test

--- src/main/java/com/prashanth/junit/experiments/Library.java
+++ src/main/java/com/prashanth/junit/experiments/Library.java
@@ -4,4 +4,16 @@ public class Library {
     public String someLibraryMethod() {
         return "Joe";
     }
+
+    boolean isPalindrome(String value) {
+        String reverseString = "";
+        for (int i = value.length() - 1; i >= 0; i-- ) {
+            reverseString = reverseString + value.charAt(i);
+        }
+
+        if(reverseString.equalsIgnoreCase(value)) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/com/prashanth/junit/experiments/LibraryTest.java
+++ src/test/java/com/prashanth/junit/experiments/LibraryTest.java
@@ -1,14 +1,29 @@
 package com.prashanth.junit.experiments;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.prashanth.junit.experiments.Library;
 
+@DisplayName("Testing Libaray class")
 public class LibraryTest {
+
+    @DisplayName("Testing someLibraryMethod")
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
         assertEquals("Joe", classUnderTest.someLibraryMethod());
+    }
+
+    @DisplayName("Testing isPalindrome")
+    @ParameterizedTest
+    @ValueSource(strings = {"Stats", "kayak", "Hanah"})
+    void testPalindromes(String value){
+        Library classUnderTest = new Library();
+        assertTrue(classUnderTest.isPalindrome(value));
     }
 }
