047869bb846ffd9583a33b7f7a4a67ab92494901
prashanth-g <prashanthcsgce@gmail.com>
2018-10-28 10:49:29 +0530

    Added dynamic test

--- src/test/java/com/prashanth/junit/experiments/DynamicTests.java
+++ src/test/java/com/prashanth/junit/experiments/DynamicTests.java
@@ -1,16 +1,26 @@
 package com.prashanth.junit.experiments;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+@DisplayName("Testing Dynamic test feature \uD83D\uDE4C")
 public class DynamicTests {
 
     @TestFactory
-    List<String> dynamicTestsWithInvalidReturnType() {
-        return Arrays.asList("Hello");
+    Collection<DynamicTest> dynamicTests() {
+        return Arrays.asList(
+                dynamicTest("DT 01", () -> assertEquals(10, 5*2)),
+                dynamicTest("DT 01", () -> assertTrue(true )),
+                dynamicTest("DT 01", () -> assertEquals(20, 10*2))
+        );
     }
 
 }
